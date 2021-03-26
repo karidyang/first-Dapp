47734ae255405d743f39c4b6d3f620cdb095404b
杨珏 <karidyang@gmail.com>
2021-03-26 16:07:15 +0800

    commit

---
+++ README.md
@@ -0,0 +1,8 @@
+* 部署
+```truffle deploy```
+
+* 启动
+```shell
+cd app
+npm run dev
+```
---
+++ contracts/GuessGame.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract GuessGame {
+  event GuessResult(uint playerChoice, uint computerChoice, uint result);
+
+  function play(uint playerChoice, uint computerChoice) public payable returns (bool) {
+    if (playerChoice > 0 && playerChoice <= 3 && computerChoice > 0 && computerChoice <= 3) {
+      if (playerChoice == computerChoice) {
+        emit GuessResult(playerChoice, computerChoice, 1);
+      } else if (playerChoice == (computerChoice + 1) % 3) {
+        emit GuessResult(playerChoice, computerChoice, 2);
+      } else {
+        emit GuessResult(playerChoice, computerChoice, 3);
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function test() public pure returns (bool) {
+    return true;
+  }
+
+}
